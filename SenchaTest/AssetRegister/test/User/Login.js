0b5b2ebc75fdb2a01334cb41c96cf61990d531c4
shogo tokita <shogo.tokita@a-saas.com>
2020-06-08 18:42:47 +0900

    Lab: Test the Login Page

--- SenchaTest/AssetRegister/test/User/Login.js
+++ SenchaTest/AssetRegister/test/User/Login.js
@@ -62,5 +62,22 @@ describe("Login", function () {
                expect(url).toContain('Default.aspx');
             });
     });
+    
+    it("should link registration page", function () {
+        ST.element('@register')
+            .click()
+            .getUrl(function (url) {
+               expect(url).toContain('Register.aspx');
+            });
+    });
+
+    it("should navigate to the login page from the registration page", function () {
+        ST.navigate('https://se.sencha.com/AssetRegister/Register.aspx')
+            .element('@login')
+            .click()
+            .getUrl(function(url) {
+                expect(url).toContain('Login.aspx');
+            });
+    });
 
 });
