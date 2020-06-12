80fb0a76f81b0aa5da3aa45ba4e772c643d80d01
shogo tokita <shogo.tokita@a-saas.com>
2020-06-12 09:12:49 +0900

    test

--- SenchaTest/AssetRegister/test/User/Assets.js
+++ SenchaTest/AssetRegister/test/User/Assets.js
@@ -1,7 +1,7 @@
 describe("Assets", function () {
     var Assets = PageObjects.Assets;
     var Dashboard = PageObjects.Dashboard;
-    
+    //test
     var newAsset = {
         name: 'Epson PowerLite Home Cinema 1080p Projector',
         description: '2D and 3D 1080p projector with 3-chip optical engine',
@@ -13,9 +13,8 @@ describe("Assets", function () {
     beforeAll(function () {
         PageObjects.login();
     });
-    
-    beforeEach(function() {
-        
+
+    beforeEach(function () {
         Assets.menuAsset()
             .click();
             
@@ -24,14 +23,13 @@ describe("Assets", function () {
     });
 
     it("should show asset data", function () {
-            
         Assets.grid()
             .rowWith('name', 'Apple iPhone 7');
             
         Assets.grid()
             .rowWith('name', 'Statesman 10-piece conference table and chairs');
     });
-    
+
     it("should show the details form when a row is clicked", function () {
         Assets.grid()
             .rowWith('name', 'Apple iPhone 7')
@@ -40,9 +38,8 @@ describe("Assets", function () {
         Assets.form()
             .visible();
     });
-    
-    it('should return to the grid from a details form when the "back" button is clicked' , function() {
-        
+
+    it("should return to the grid from a details form when the \"back\" button is clicked", function () {
         Assets.grid()
             .rowWith('name', 'Apple iPhone 7')
             .click();
@@ -55,20 +52,17 @@ describe("Assets", function () {
             
         Assets.grid()
             .visible();
-            
     });
-    
-    it ('should visible Asset Details form when the "Add New" button is clicked', function() {
-        
+
+    it("should visible Asset Details form when the \"Add New\" button is clicked", function () {
         Assets.addButton()
             .click();
         
         Assets.form()
             .visible();
     });
-    
-    it('should show an error message when an asset details form that is missing a value is saved', function(){
 
+    it("should show an error message when an asset details form that is missing a value is saved", function () {
         // TODO: Click the add button
         Assets.addButton()
             .click();
@@ -81,7 +75,7 @@ describe("Assets", function () {
         // TODO: Click the save button
         Assets.saveButton()
             .click();
-
+        
         Assets.messageBox()
             .visible()
             .textLike('Validation errors')
@@ -96,20 +90,18 @@ describe("Assets", function () {
             
         Assets.messageBox()
             .hidden();
-
     });
-    
-    it('should initialize new asset fields to all empty values, except date, which is the current date', function() {
 
+    it("should initialize new asset fields to all empty values, except date, which is the current date", function () {
         Assets.addButton()
             .click();
             
         Assets.form()
             .visible();
-    
+        
         Assets.nameField()
             .valueEmpty();
-    
+        
         // TODO: Verify that the description field is empty
         Assets.descriptionField()
             .valueEmpty();
@@ -117,14 +109,14 @@ describe("Assets", function () {
         // TODO: Verify that the type field is empty
         Assets.typeField()
             .valueEmpty();
-    
+        
         Assets.quantityField()
             .value(0);
-    
+        
         // TODO: Verify that the cost field is 0
         Assets.costField()
             .value(0);
-    
+        
         var dateString = new Date().toLocaleDateString("en-US", {
             year: "numeric",
             month: "2-digit",
@@ -133,18 +125,16 @@ describe("Assets", function () {
         
         Assets.dateField()
             .expect('formattedValue').toEqual(dateString);
-    
+        
         Assets.backButton();
-    
     });
-    
-    it('should allow every field on the asset details form to be edited', function() {
 
+    it("should allow every field on the asset details form to be edited", function () {
         Assets.addButton()
             .click();
         Assets.form()
             .visible();
-    
+        
         // TODO: Set the name field to newAsset.name
         Assets.nameField()
             .setValue(newAsset.name);
@@ -178,15 +168,13 @@ describe("Assets", function () {
             .cellWith('dataIndex', 'cost')
             .text(formattedCost);
         ST.log(formattedCost);
-
     });
-    
-    it('should let the user know when data has been saved,', function() {
-    
+
+    it("should let the user know when data has been saved,", function () {
         Assets.grid()
             .rowWith('name', 'Statesman 10-piece conference table and chairs')
             .click();
-    
+        
         // TODO: Wait for the form to be visible
         Assets.form()
             .visible();
@@ -203,15 +191,13 @@ describe("Assets", function () {
         Assets.messageToast()
             .visible()
             .textLike('Record saved');
-    
     });
-    
-    it('should show a delete confirmation', function() {
 
+    it("should show a delete confirmation", function () {
         Assets.grid()
             .rowWith('name', 'Statesman 10-piece conference table and chairs')
             .click();
-    
+        
         // TODO: Wait for the form to be visible
         Assets.form()
             .visible()
@@ -238,7 +224,5 @@ describe("Assets", function () {
             .visible()
             .rowWith('name', 'Statesman 10-piece conference table and chairs')
             .timedout();
-    
     });
-
 });
